5c6350a4cf31123c83e2a6e5b08984c6be702861
AmirHossein Abdolmotallebi <amir.a1376@gmail.com>
2025-02-18 16:37:55 +0330

    sort languages

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -2,16 +2,16 @@ import ar_SA from "./locales/ar-SA.json";
 import bn_BD from "./locales/bn-BD.json";
 import de_DE from "./locales/de-DE.json";
 import en_US from "./locales/en-US.json";
+import es_ES from "./locales/es-ES.json";
 import fa_IR from "./locales/fa-IR.json";
 import fr_FR from "./locales/fr-FR.json";
-import it_IT from "./locales/it-IT.json";
 import id_ID from "./locales/id-ID.json";
+import it_IT from "./locales/it-IT.json";
 import pt_BR from "./locales/pt-BR.json";
+import ru_RU from "./locales/ru-RU.json";
 import tr_TR from "./locales/tr-TR.json";
 import uk_UA from "./locales/uk-UA.json";
-import ru_RU from "./locales/ru-RU.json";
 import vi_VN from "./locales/vi-VN.json";
-import es_ES from "./locales/es-ES.json";
 import zh_CN from "./locales/zh-CN.json";
 import zh_TW from "./locales/zh-TW.json";
 import { addLocales, setDefaultLocale } from "./TranslationRegistry.ts";
@@ -28,9 +28,9 @@ export function setUpIntl() {
     "id-ID":id_ID,
     "it-IT":it_IT,
     "pt-BR":pt_BR,
-    "uk-UA":uk_UA,
     "ru-RU":ru_RU,
     "tr-TR":tr_TR,
+    "uk-UA":uk_UA,
     "vi-VN":vi_VN,
     "zh-CN":zh_CN,
     "zh-TW":zh_TW,

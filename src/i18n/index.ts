daf4f548bb370f342413255f43fac75c62e98164
AmirHossein Abdolmotallebi <amir.a1376@gmail.com>
2025-05-13 23:51:34 +0330

    add Finnish language

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -4,6 +4,7 @@ import de_DE from "./locales/de-DE.json";
 import en_US from "./locales/en-US.json";
 import es_ES from "./locales/es-ES.json";
 import fa_IR from "./locales/fa-IR.json";
+import fi_FI from "./locales/fi-FI.json";
 import fr_FR from "./locales/fr-FR.json";
 import id_ID from "./locales/id-ID.json";
 import jp_JP from "./locales/ja-JP.json";
@@ -25,6 +26,7 @@ export function setUpIntl() {
     "en-US":en_US,
     "es-ES":es_ES,
     "fa-IR":fa_IR,
+    "fi-FI":fi_FI,
     "fr-FR":fr_FR,
     "id-ID":id_ID,
     "it-IT":it_IT,

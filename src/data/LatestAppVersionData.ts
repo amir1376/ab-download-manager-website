ec0af7c3850c237703a3a0a255aa0ad26029cbf6
AmirHossein Abdolmotallebi <amir.a1376@gmail.com>
2025-05-13 18:09:11 +0330

    Update LatestAppVersionData.ts

--- src/data/LatestAppVersionData.ts
+++ src/data/LatestAppVersionData.ts
@@ -150,7 +150,7 @@ const defaultVersionData: {
 ]
 
 const experimentalPlatforms:PossiblePlatformsType[] = [
-    "linux"
+    "linux", "mac"
 ]
 
 function isPlatformExperimental(platform:PossiblePlatformsType){

6b4eb0b4e3683b0b2774a37e17a510a3fcdbb527
AmirHossein Abdolmotallebi <amir.a1376@gmail.com>
2026-06-02 11:56:10 +0330

    update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,19 @@
 import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import path from "path"
+import * as fs from "node:fs";
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [
-        react()
+        react(),
+        // GitHub pages doesn't support SPA by default, this is a workaround
+        {
+            name: "copy-404",
+            closeBundle() {
+                fs.copyFileSync("dist/index.html", "dist/404.html");
+                console.log("404.html created from index.html");
+            },
+        },
     ],
     resolve: {
         alias: {

87ea2a61c3a6ea98c94dc794019a0dfe91c2ae45
AmirHossein Abdolmotallebi <amir.a1376@gmail.com>
2026-06-03 19:16:31 +0330

    prepare the website for cloudflare pages (#26)

--- vite.config.ts
+++ vite.config.ts
@@ -6,14 +6,6 @@ import * as fs from "node:fs";
 export default defineConfig({
     plugins: [
         react(),
-        // GitHub pages doesn't support SPA by default, this is a workaround
-        {
-            name: "copy-404",
-            closeBundle() {
-                fs.copyFileSync("dist/index.html", "dist/404.html");
-                console.log("404.html created from index.html");
-            },
-        },
     ],
     resolve: {
         alias: {

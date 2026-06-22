866b3c0fb7e35dbd948e38dea49490ef08024944
AmirHossein Abdolmotallebi <amir.a1376@gmail.com>
2026-06-22 15:06:12 +0330

    remove experimental status

--- src/data/LatestAppVersionData.ts
+++ src/data/LatestAppVersionData.ts
@@ -161,7 +161,7 @@ const defaultVersionData: {
 ]
 
 const experimentalPlatforms: PossiblePlatformsType[] = [
-    "linux", "mac"
+
 ]
 
 function isPlatformExperimental(platform: PossiblePlatformsType) {
